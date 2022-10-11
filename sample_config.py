bc6da7a7fd3dc4d9c834ef7648317942527dbb6e
YASEEN15 <114953858+YASEEN15@users.noreply.github.com>
2022-10-11 20:40:36 +0200

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,7 +2,7 @@ import os
 from typing import Set
 class Config(object):
     LOGGER = True
-    ALIVE_NAME = os.environ.get("ALIVE_NAME", "@IQTHON")
+    ALIVE_NAME = os.environ.get("ALIVE_NAME", "@y_a_n_u_u")
     APP_ID = int(os.environ.get("APP_ID", 6))
     API_HASH = os.environ.get("API_HASH") or None
     DB_URI = os.environ.get("DATABASE_URL", None)
@@ -11,7 +11,7 @@ class Config(object):
     TG_BOT_USERNAME = None
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO", "https://github.com/telethonArab/telethonAr1")
-    AUTONAME = os.environ.get("AUTONAME", "@iqthon")
+    AUTONAME = os.environ.get("AUTONAME", "@y_a_n_u_u")
     PRIVATE_GROUP_BOT_API_ID = int(os.environ.get("PRIVATE_GROUP_BOT_API_ID") or 0)
     PRIVATE_GROUP_ID = int(os.environ.get("PRIVATE_GROUP_ID") or 0)
     PRIVATE_CHANNEL_BOT_API_ID = int(os.environ.get("PRIVATE_CHANNEL_BOT_API_ID") or 0)
@@ -21,7 +21,7 @@ class Config(object):
     PM_LOGGER_GROUP_ID = int( os.environ.get("PM_LOGGER_GROUP_ID") or os.environ.get("PM_LOGGR_BOT_API_ID") or 0 )
     PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL") or 0)
     TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "iqthon")
-    THUMB_IMAGE = os.environ.get("THUMB_IMAGE", "https://telegra.ph/file/f1b9e7e1a5b0f719bc5d9.jpg")
+    THUMB_IMAGE = os.environ.get("THUMB_IMAGE", "https://telegra.ph/file/979fe639e5e6b325c8c68.jpg")
     NO_LOAD = [x for x in os.environ.get("NO_LOAD", "").split()]
     DIGITAL_PIC = os.environ.get("DIGITAL_PIC", None)
     DEFAULT_PIC = os.environ.get("DEFAULT_PIC", None)
