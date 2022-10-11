815f758382a4bc954c819f92e18e3fc8b16be4a7
YASEEN15 <114953858+YASEEN15@users.noreply.github.com>
2022-10-11 22:45:34 +0200

    Add files via upload

---
+++ SQL/extras/Source.py
@@ -0,0 +1,11 @@
+import re
+import time
+from datetime import datetime
+from Arab import StartTime, iqthon
+from Arab.Config import Config
+from Arab.plugins import mention
+help1 = ("**🝳 ⦙ كيفيه التنصيب :**")
+help2 = ("**🝳 ⦙ قـائمـه الاوامـر :**\n**🝳 ⦙ قنـاه السـورس :** @y_a_n_u_u\n**🝳 ⦙ شـرح اوامـر السـورس : @y_a_n_u_u**\n**🝳 ⦙ شـرح فـارات السـورس : @y_a_n_u_u** \n - اوامر الاونلاين تشتغل فقط في المجموعات ")
+TG_BOT = Config.TG_BOT_USERNAME
+TM = time.strftime("%I:%M")
+Sour = f"**‎⿻┊My 𖠄 {mention} ٫ **\n‌‎**⿻┊BoT 𖠄 {TG_BOT} ٫**\n**‌‎⿻┊TimE 𖠄 {TM} ٫**\n**‌‎⿻┊‌‎VeRsIoN 𖠄 (7.7) ,** \n**⿻┊‌‎YASEEN00𖠄** @y_a_n_u_u"
